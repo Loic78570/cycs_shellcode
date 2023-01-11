5980753aa1f42eba6eff222a28b2d5420de6b4f5
Loïc Gavens <loic.2010@live.fr>
2023-01-11 17:24:46 +0100

    still segvaults

--- wrapper/wrapper.c
+++ wrapper/wrapper.c
@@ -20,9 +20,11 @@ int main(int argc,  char *argv[]){
 
     ret();
 //
-////    const char *argvx = ["/bin/sh", "lol"];
+//    const char *argvx = ["/bin/sh", "lol"];
 //
 //    execve("/bin/sh", NULL, NULL);
+
+//  cmake-build-debug/wrapper $(python3 -c 'print("\x48\x31\xf6\x56\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5f\xb0\x3b\x99\x0f\x05")')
 
 
 }
