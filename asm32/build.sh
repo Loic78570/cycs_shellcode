9caeb3989eba4b4e6aa1b6f17f14a7f1ae23aa1a
Loïc Gavens <loic.2010@live.fr>
2023-01-12 13:43:51 +0100

    build working

--- asm32/build.sh
+++ asm32/build.sh
@@ -7,6 +7,7 @@ do
 nasm -f elf32 "$arg" -o com32.o
 #ld -m elf32 com32.o -o a01
 ld -m elf_i386 -s -o a01 asm01.o
+echo "C'est build ! (./a01)"
 #./a01
 
 done
--- asm64/build.sh
+++ asm64/build.sh
@@ -7,7 +7,7 @@ do
 nasm -f elf64 "$arg" -o com32.o
 #ld -m elf32 com32.o -o a01
 ld -m elf_x86_64 -s -o a01 asm01.o
-echo "C'est build!"
+echo "C'est build! (./a01)"
 #./a01
 
 done
