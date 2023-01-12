ec07aa9fd7adbbe7cb79a5c60187e47e767e4f3d
Loïc Gavens <loic.2010@live.fr>
2023-01-12 15:41:20 +0100

    asm3 32

--- asm32/asm03.s
+++ asm32/asm03.s
@@ -1,27 +1,37 @@
 section .text
 
+
     global _start
 
 _start:
 
         ; write our string to stdout.
+        mov eax,42 ;
+        cmp eax,42 ;
+	    je print  ;
+        call    end1
 
-        call    print
-        call    end
-
-end:
+end0:
 
 	    mov	    ebx,0	  ; first syscall argument: exit code.
+        mov     eax,1     ; system call number (sys_exit).
+        int     0x80	  ; call kernel.
+
+end1:
+
+	    mov	    ebx,1	  ; first syscall argument: exit code.
         mov     eax,1     ; system call number (sys_exit).
         int     0x80	  ; call kernel.
 
 print:
 
         mov     edx,len   ; third argument: message length.
-        mov     ecx,msg   ; second argument: pointer to message to write.
+        mov     ecx,[esp + 4]   ; second argument: pointer to message to write.
+        mov     ecx,[esp + 4]   ; second argument: pointer to message to write.
         mov     ebx,1     ; first argument: file handle (stdout).
         mov     eax,4     ; system call number (sys_write).
         int     0x80	  ; call kernel.
+        call    end0
         ret
 
 section .data
