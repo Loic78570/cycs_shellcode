e3f69169bb9ebabe5bf6d8bdf9b16c350a2d8082
Loïc Gavens <loic.2010@live.fr>
2023-01-12 17:04:59 +0100

    c file associated

--- test.c
+++ test.c
@@ -1,3 +1,14 @@
-int main(){
-    return 0;
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Entrez un entier: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
