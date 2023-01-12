bb32635eda252e5045802d6a2a8c3a53185721f7
Loïc Gavens <loic.2010@live.fr>
2023-01-12 13:39:52 +0100

    build working

---
+++ asm32/build.sh
@@ -0,0 +1,12 @@
+#nasm -f elf32 asm01.s -o asm01.o
+#ld asm01.o -o a01
+
+for arg in "$@"
+do
+
+nasm -f elf32 "$arg" -o com32.o
+#ld -m elf32 com32.o -o a01
+ld -m elf_i386 -s -o a01 asm01.o
+./a01
+
+done
---
+++ asm64/build.sh
@@ -0,0 +1,3 @@
+#nasm -f elf64 asm01.s -o asm01.o
+#ld -s -o a01 asm01.o
+
