55c01d165c0fb0d6723d3106f141bce95ada4337
Loïc Gavens <loic.2010@live.fr>
2023-01-12 16:01:56 +0100

    build 64 update

--- asm64/build.sh
+++ asm64/build.sh
@@ -5,7 +5,7 @@ for arg in "$@"
 do
 
 #nasm -f elf64 "$arg" -o com32.o
-nasm -felf64 "$arg"
+nasm -felf64 "$arg" -o testz.o
 #ld -m elf32 com32.o -o a01
 #ld -m elf_x86_64 -s -o a01 asm01.o
 ld testz.o
