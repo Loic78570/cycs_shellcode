7d10a1a614a97a3bd377e641fec8e3e34a5b5bdc
Loïc Gavens <loic.2010@live.fr>
2023-01-12 13:51:09 +0100

    assembly ready updated build

--- asm64/build.sh
+++ asm64/build.sh
@@ -4,10 +4,13 @@
 for arg in "$@"
 do
 
-nasm -f elf64 "$arg" -o com32.o
+#nasm -f elf64 "$arg" -o com32.o
+nasm -felf64 "$arg"
 #ld -m elf32 com32.o -o a01
-ld -m elf_x86_64 -s -o a01 asm01.o
-echo "C'est build! (./a01)"
+#ld -m elf_x86_64 -s -o a01 asm01.o
+ld testz.o
+
+echo "C'est build! (./a.out)"
 #./a01
 
 done
