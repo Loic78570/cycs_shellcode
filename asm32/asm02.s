57f4fe34eec1d4053fb1ab0c31b3239cea3a441a
Loïc Gavens <loic.2010@live.fr>
2023-01-12 14:17:11 +0100

    asm02 32 ok

--- asm32/asm02.s
+++ asm32/asm02.s
@@ -1,14 +1,24 @@
-          global    _start
+section .text
 
-          section   .text
-_start:   mov       eax, 1                  ;
-          mov       edi, 1                  ;
-          mov       esi, message            ;
-          mov       edx, 5                  ;
-          syscall                           ;
-          mov       eax, 60                 ;
-          xor       edi, edi                ;
-          syscall                           ;
+    global _start
 
-          section   .data
-message:  db        "1337", 4               ;
+_start:
+
+        ; write our string to stdout.
+
+        mov     edx,len   ; third argument: message length.
+        mov     ecx,msg   ; second argument: pointer to message to write.
+        mov     ebx,1     ; first argument: file handle (stdout).
+        mov     eax,4     ; system call number (sys_write).
+        int     0x80	  ; call kernel.
+
+        ; and exit.
+
+	    mov	ebx,0	      ; first syscall argument: exit code.
+        mov     eax,1     ; system call number (sys_exit).
+        int     0x80	  ; call kernel.
+
+section .data
+
+msg     db      "1337",0xa
+len     equ     $ - msg
