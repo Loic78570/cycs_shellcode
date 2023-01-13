9b08313baedef146ea879f977b5ec4130f7fede4
Loic78570 <loic.2010@live.fr>
2023-01-13 13:48:27 +0100

    create_shellcode (mais fait avant)

---
+++ create_shellcode.sh
@@ -0,0 +1,7 @@
+echo "Dumping shellcodes in progress..."
+for i in "$@";
+do
+  echo "Shellcode for $arg"
+  echo $(objdump -d $arg | grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g' )
+)
+done
